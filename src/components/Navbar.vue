6b571e50af34e48250190a719bc40ecc70acf0fe
spdevuk@gmail.com <spdevuk@gmail.com>
2019-09-17 15:58:12 +0100

    feat: add basic navbar with links to pages/github

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-menu
+    :default-active="$route.path"
+    class="el-menu-demo"
+    mode="horizontal"
+    router
+  >
+    <el-menu-item index="/">
+      <router-link to="/">
+        Home
+      </router-link>
+    </el-menu-item>
+    <el-menu-item index="/about">
+      <router-link to="/about">
+        About
+      </router-link>
+    </el-menu-item>
+    <el-menu-item @click="visitGithub">
+      <a href="#"> Github</a>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  methods: {
+    visitGithub() {
+      window.open("https://github.com/SPDUK/vue-todos", "_blank");
+    }
+  }
+};
+</script>
